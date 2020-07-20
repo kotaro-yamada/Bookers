d119f64e76707b708ca14b641520fa31eb41aee2
vagrant <vagrant@localhost.localdomain>
2020-07-20 04:03:31 +0000

    Completed3rdExercise

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,15 @@ class FavoritesController < ApplicationController
 		book = Book.find(params[:book_id])
 		favorite = current_user.favorites.new(book_id: book.id)
 		favorite.save
-		redirect_to books_path
+		path = Rails.application.routes.recognize_path(request.referer)
+		redirect_to path
 	end
 
 	def destroy
 		book = Book.find(params[:book_id])
 		favorite = current_user.favorites.find_by(book_id: book.id)
 		favorite.destroy
-		redirect_to books_path
+		path = Rails.application.routes.recognize_path(request.referer)
+		redirect_to path
 	end
 end
